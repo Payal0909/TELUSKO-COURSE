21878a4d6f35a116842b7bc5b8babb0c79d4bbce
payal.bhawnani <payal.bhawnani@addverb.com>
2024-03-17 20:11:11 +0530

    long and float in java

--- src/introOfJava/Variables.java
+++ src/introOfJava/Variables.java
@@ -4,6 +4,10 @@ public class Variables {
 
     public static void main(String[] args) {
         int num1=10,num2=12;
+        float num=5.8f;
+        long l = 589l;
         System.out.println(num1+num2);
+        System.out.println(l);
+        System.out.println(num);
     }
 }
