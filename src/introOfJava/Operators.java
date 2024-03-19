4c0e6896016e6a82283759f811d19b0d4bd97c19
payal.bhawnani <payal.bhawnani@addverb.com>
2024-03-19 21:41:51 +0530

    Operators in Java

--- src/introOfJava/Operators.java
+++ src/introOfJava/Operators.java
@@ -3,12 +3,18 @@ package introOfJava;
 public class Operators {
     public static void main(String[] args) {
         int num =5;
+        int num2 = 6;
+        //ARITHMETIC
         // post increment
         int a = num++;  // num is assigned as 5 in a and then incremented to 6
         System.out.println(a);
        // pre increment
         int b = ++num; // here num is first incremented to 7 then assigned to b
         System.out.println(b);
+
+        //RELATIONAL
+         boolean result = num<num2;
+        System.out.println(result);
 
     }
 }
