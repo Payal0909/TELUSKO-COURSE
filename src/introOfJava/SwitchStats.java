d38d25f3067b1369477c418b8d8642055c3bfd42
Payal Bhawnani <bhawnanipayalgsd09@gmail.com>
2024-04-23 17:35:23 +0530

    switch statements

---
+++ src/introOfJava/SwitchStats.java
@@ -0,0 +1,23 @@
+package introOfJava;
+
+public class SwitchStats {
+
+    public static void main(String[] args) {
+
+//        switch statements
+
+        int n=2;
+        switch (n)
+        {
+            case 1:
+                System.out.println("Dinky");
+                break;
+            case 2 :
+                System.out.println("Simmy");
+                break;
+            default :
+            System.out.println("came out of Switch");
+
+        }
+    }
+}
