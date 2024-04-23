e496067348e4d4efd48bdc7e18a7a7282fd9955c
Payal Bhawnani <bhawnanipayalgsd09@gmail.com>
2024-04-23 16:30:48 +0530

    ternary operator

--- src/introOfJava/ConditionalStats.java
+++ src/introOfJava/ConditionalStats.java
@@ -2,6 +2,15 @@ package introOfJava;
 
 public class ConditionalStats {
 
-//    if elseif else statements
+    public static void main(String[] args) {
+        //    if elseif else statements
 
+        //    ternary operator
+        int n=11;
+        int res = n %2 ==0 ? 10 :20;
+        System.out.println(res);
+
+    }
 }
+
+
