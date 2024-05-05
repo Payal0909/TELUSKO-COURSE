03bd0a387bffc60d6b82fb92969b3cbf62cce2cf
Payal Bhawnani <bhawnanipayalgsd09@gmail.com>
2024-05-05 15:47:30 +0530

    Jagged Array

--- src/introOfJava/Arrays.java
+++ src/introOfJava/Arrays.java
@@ -31,5 +31,32 @@ public class Arrays {
             }
             System.out.println();
         }
+
+//         Jagged Array
+
+        int num4[][]= new int[3][];
+        num4[0]=new int[4];
+        num4[1]=new int[2];
+        num4[2]=new int[3];
+
+        for(int n[]:num4)
+        {
+            for(int m :n)
+            {
+                m=(int) (Math.random()*10);
+                System.out.print(m);
+            }
+            System.out.println();
+        }
+
+        for(int i=0;i< num4.length;i++)
+        {
+            for(int j=0;j<num4[i].length;j++)
+            {
+                num4[i][j] = (int) (Math.random()*10);
+                System.out.print(num4[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
