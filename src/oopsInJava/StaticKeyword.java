cf312aab54aaa38f78f98ad7fabb56ebea3c8c67
Payal Bhawnani <bhawnanipayalgsd09@gmail.com>
2024-05-25 21:19:44 +0530

    Static Variable

---
+++ src/oopsInJava/StaticKeyword.java
@@ -0,0 +1,32 @@
+package oopsInJava;
+
+class Mobile{
+    String name;
+    String brand;
+    static String type;
+}
+public class StaticKeyword {
+    public static void main(String[] args) {
+        // Static variable of a class is shared by all its objects
+        // Static variable can be called by its Method directly
+        // there is aa different area in JVM where these static variables are created
+
+        Mobile m1=new Mobile();
+        m1.brand="M1";
+        m1.name="oppo";
+        Mobile.type="Android";
+
+        Mobile m2=new Mobile();
+        m2.brand="M2";
+        m2.name="oppo";
+
+        System.out.println(m1.brand + ": " + Mobile.type);
+        System.out.println(m2.brand + ": " + Mobile.type);
+
+        m2.type="Mac";
+        System.out.println(m1.brand + ": " + Mobile.type);
+        System.out.println(m2.brand + ": " + Mobile.type);
+
+    }
+
+}
