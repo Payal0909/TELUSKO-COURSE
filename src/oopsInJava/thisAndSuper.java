ae97c95426d18f5916d1afccf59262a79e72d3ce
Payal Bhawnani <bhawnanipayalgsd09@gmail.com>
2024-06-09 23:46:13 +0530

    this  and super method

---
+++ src/oopsInJava/thisAndSuper.java
@@ -0,0 +1,18 @@
+package oopsInJava;
+
+public class thisAndSuper {
+    public static void main(String[] args) {
+
+        // Access modifier
+        // this and super method
+
+
+        // Whenever you will create an object of subclass then constructor of both super and subclass gets called
+
+        // Every constructor in java has a method called super() which means call the constructor of super class
+
+        // every class in java extends Objects class
+
+        // this method execute the method of the same class
+    }
+}
