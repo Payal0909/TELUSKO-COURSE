1fdb8b90d225091de3c3871af755ecdae420435a
Payal Bhawnani <bhawnanipayalgsd09@gmail.com>
2024-06-12 21:44:22 +0530

    Object Class

---
+++ src/oopsInJava/finalKeyword.java
@@ -0,0 +1,14 @@
+package oopsInJava;
+
+public class finalKeyword {
+    public static void main(String[] args) {
+
+     // final keyword is used with variable to make it constant ,that is the value can't be changed later
+
+        // when a class is made final then it means no class can extend that final class and
+        // inheritance is stopped
+
+
+//        when a method is made final it means that no one can override it
+    }
+}
