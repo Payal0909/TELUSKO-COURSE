468a0229534064ff41b7801206c0fea5be0b235b
Payal Bhawnani <bhawnanipayalgsd09@gmail.com>
2024-06-13 12:15:06 +0530

    Abstract Keyword

---
+++ src/oopsInJava/AbstractKeyword.java
@@ -0,0 +1,16 @@
+package oopsInJava;
+
+public class AbstractKeyword {
+    public static void main(String[] args) {
+
+        // Abstract keyword
+//        abstract method can only be defined under abstract class
+//      and the child class should contain the parent class declared function definitions
+//        i.e. if we are extending the abstract class we need to define all its super class declared functions
+//    Cannot create object of the abstract car
+
+//        Abstract class can or cant have abstract method . there is no compulsion of that
+
+    }
+
+}
