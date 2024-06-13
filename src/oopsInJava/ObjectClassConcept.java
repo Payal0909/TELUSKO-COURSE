cbbffff07d0bb9ef76bbf475520e22284e528ff2
Payal Bhawnani <bhawnanipayalgsd09@gmail.com>
2024-06-13 09:52:46 +0530

    ObjectClassConcept

---
+++ src/oopsInJava/ObjectClassConcept.java
@@ -0,0 +1,17 @@
+package oopsInJava;
+
+public class ObjectClassConcept {
+    public static void main(String[] args) {
+
+        // Everytime we try to print the object it calls toString() method
+//        Also we can override the method by creating a toString() method of our own
+
+//        obj1.equals(obj2); This compares the hash value of obj not the values which obj fields contain
+//        .hashCode() is used to generate the hashCode of the function
+
+//        .equals() used to compare String values
+
+
+    }
+
+}
